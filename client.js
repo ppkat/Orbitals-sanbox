2b70e9644d2095f989ed6082a18e956254c930df
João Pedro <joaopedro@sobremail.com>
2022-08-28 14:57:22 -0300

    fix: aceleration calc

--- client.js
+++ client.js
@@ -24,7 +24,11 @@ function createGame() {
         state.push(circle)
     }
 
-    function gravity(body1, body2) {
+    function gravity(body1, body2, i) {
+
+        if(!i) {
+            body1.acelerationx = body2.acelerationx = body1.acelerationy = body2.acelerationy = 0
+        }
 
         console.log(body1, body2)
         const gravityConstant = 6.67408 * 10 ** -11
@@ -36,17 +40,18 @@ function createGame() {
         const distancey = center1.y - center2.y
 
         const distance = Math.sqrt(distancex ** 2 + distancey ** 2) // d² = (x1 - x2)² + (y1 - y2)²
-        const gravityForce = /*gravityConstant*/ mass1 * mass2 / distance ** 2 // F = G * m * m) / d²
+        const gravityForce = gravityConstant* mass1 * mass2 / distance ** 2 // F = G * m * m) / d²
+        console.log(gravityForce)
 
         const sin = distancey / distance
         const cos = distancex / distance
         const forcex = gravityForce * cos
         const forcey = gravityForce * sin
 
-        body1.acelerationx = -forcex / mass1 // F = m * a
-        body1.acelerationy = -forcey / mass1
-        body2.acelerationx = forcex / mass2
-        body2.acelerationy = forcey / mass2
+        body1.acelerationx += -forcex / mass1 // F = m * a
+        body1.acelerationy += -forcey / mass1
+        body2.acelerationx += forcex / mass2
+        body2.acelerationy += forcey / mass2
 
     }
 
@@ -73,11 +78,11 @@ function createGame() {
         }
     }
 
-    createRetangle({ mass: 1000, color: 'red', x: 50, y: 200, width: 30, height: 70 })
-    createRetangle({ mass: 800000000, color: 'yellow', x: 400, y: 100, width: 200, height: 170 })
-    createRetangle({ mass: 5000, color: 'red', x: 200, y: 300, width: 50, height: 75 })
-    createCircle({ color: 'brown', mass: 4000, x: 800, y: 80, radius: 70, speedx: -5 })
-    createCircle({ color: 'purple', mass: 8000, x: 50, y: 500, radius: 70 })
+    createRetangle({ mass: 10000000, color: 'red', x: 50, y: 200, width: 30, height: 70 })
+    createRetangle({ mass: 80000000000000, color: 'yellow', x: 400, y: 100, width: 200, height: 170 })
+    createRetangle({ mass: 50000000, color: 'red', x: 200, y: 300, width: 50, height: 75 })
+    createCircle({ color: 'brown', mass: 4000000000, x: 800, y: 80, radius: 70, speedx: -7 })
+    createCircle({ color: 'purple', mass: 80000000000000, x: 50, y: 500, radius: 70, speedx: 3 })
 
     return {
         state,
@@ -98,7 +103,7 @@ function render() {
 
         for (let j = i + 1; j < game.state.length; j++) {
             const body2 = game.state[j]
-            game.gravity(body, body2)
+            game.gravity(body, body2, i)
         }
 
         game.move(body)
