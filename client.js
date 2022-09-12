b2bc65e6b5e42e9a8e51b85269882b911e9d7e65
João Pedro <joaopedro@sobremail.com>
2022-09-11 23:00:21 -0300

    fix: first body of state do not acelerate

--- client.js
+++ client.js
@@ -24,35 +24,35 @@ function createGame() {
         state.push(circle)
     }
 
-    function gravity(body1, body2, i) {
+    function gravity(state, i) {
+        const body1 = state[i]
+        const previousBody = state[i > 0? i - 1 : state.length - 1]
+        previousBody.acelerationx = previousBody.acelerationy = 0 //cleaning previous aceleration
 
-        if(!i) {
-            body1.acelerationx = body2.acelerationx = body1.acelerationy = body2.acelerationy = 0
+        for (let j = i + 1; j < state.length; j++) {
+            const body2 = state[j]
+
+            const gravityConstant = 6.67408 * 10 ** -11
+            const mass1 = body1.mass
+            const mass2 = body2.mass
+            const center1 = body1.format === 'circle' ? { x: body1.x, y: body1.y } : { x: body1.x + body1.width / 2, y: body1.y + body1.height / 2 }
+            const center2 = body2.format === 'circle' ? { x: body2.x, y: body2.y } : { x: body2.x + body2.width / 2, y: body2.y + body2.height / 2 }
+            const distancex = center1.x - center2.x
+            const distancey = center1.y - center2.y
+
+            const distance = Math.sqrt(distancex ** 2 + distancey ** 2) // d² = (x1 - x2)² + (y1 - y2)²
+            const gravityForce = gravityConstant * mass1 * mass2 / distance ** 2 // F = G * m * m) / d²
+
+            const sin = distancey / distance
+            const cos = distancex / distance
+            const forcex = gravityForce * cos
+            const forcey = gravityForce * sin
+
+            body1.acelerationx += -forcex / mass1 // F = m * a
+            body1.acelerationy += -forcey / mass1
+            body2.acelerationx += forcex / mass2
+            body2.acelerationy += forcey / mass2
         }
-
-        console.log(body1, body2)
-        const gravityConstant = 6.67408 * 10 ** -11
-        const mass1 = body1.mass
-        const mass2 = body2.mass
-        const center1 = body1.format === 'circle' ? { x: body1.x, y: body1.y } : { x: body1.x + body1.width / 2, y: body1.y + body1.height / 2 }
-        const center2 = body2.format === 'circle' ? { x: body2.x, y: body2.y } : { x: body2.x + body2.width / 2, y: body2.y + body2.height / 2 }
-        const distancex = center1.x - center2.x
-        const distancey = center1.y - center2.y
-
-        const distance = Math.sqrt(distancex ** 2 + distancey ** 2) // d² = (x1 - x2)² + (y1 - y2)²
-        const gravityForce = gravityConstant* mass1 * mass2 / distance ** 2 // F = G * m * m) / d²
-        console.log(gravityForce)
-
-        const sin = distancey / distance
-        const cos = distancex / distance
-        const forcex = gravityForce * cos
-        const forcey = gravityForce * sin
-
-        body1.acelerationx += -forcex / mass1 // F = m * a
-        body1.acelerationy += -forcey / mass1
-        body2.acelerationx += forcex / mass2
-        body2.acelerationy += forcey / mass2
-
     }
 
     function move(body) {
@@ -61,6 +61,25 @@ function createGame() {
         body.x += body.speedx
         body.speedy += body.acelerationy
         body.y += body.speedy
+    }
+
+    function colision(body1, body2) {
+
+        if (body1.format === 'retangle' && body2.format === 'retangle') {
+            if (body1.x <= body2.x + body2.width && body1.x + body1.width >= body2.x && body1.y <= body2.y + body2.width && body1.y + body1.width >= body2.y) {
+                body1.speedx = -body1.speedx
+                body1.speedy = -body1.speedy
+                body2.speedx = -body2.speedx
+                body2.speedy = -body2.speedy
+            }
+        }
+        else if (body1.format === 'circle' && body2.format === 'circle') {
+            //if(body1.x + body1.width >= body2.x - body2.radius)
+        }
+        else if ((body1.format === 'circle' && body2.format === 'retangle') || (body1.format === 'retangle' && body2.format === 'circle')) {
+
+        }
+
     }
 
     function draw() {
@@ -78,11 +97,14 @@ function createGame() {
         }
     }
 
-    createRetangle({ mass: 10000000, color: 'red', x: 50, y: 200, width: 30, height: 70 })
-    createRetangle({ mass: 80000000000000, color: 'yellow', x: 400, y: 100, width: 200, height: 170 })
-    createRetangle({ mass: 50000000, color: 'red', x: 200, y: 300, width: 50, height: 75 })
-    createCircle({ color: 'brown', mass: 4000000000, x: 800, y: 80, radius: 70, speedx: -7 })
-    createCircle({ color: 'purple', mass: 80000000000000, x: 50, y: 500, radius: 70, speedx: 3 })
+    createRetangle({ mass: 1 * 10 ** 7, color: 'red', x: 50, y: 200, width: 30, height: 70 })
+    createRetangle({ mass: 8 * 10 ** 13, color: 'yellow', x: 400, y: 100, width: 200, height: 170 })
+    createRetangle({ mass: 5 * 10 ** 7, color: 'red', x: 200, y: 300, width: 50, height: 75 })
+    createCircle({ color: 'brown', mass: 4 * 10 ** 13, x: 800, y: 80, radius: 50, speedx: -7 })
+    createCircle({ color: 'purple', mass: 8 * 10 ** 13, x: 50, y: 500, radius: 50, speedx: 3 })
+    createCircle({ color: 'blue', mass: 1.1 * 10 ** 14, x: 1000, y: 700, radius: 85, speedx: 0 })
+    createRetangle({ mass: 9.01 * 10 ** 13, color: 'pink', x: 750, y: 300, width: 100, height: 90, speedx: 4 })
+    createCircle({ color: 'gray', mass: 6 * 10 ** 9, x: 600, y: 800, radius: 8, speedx: 3 })
 
     return {
         state,
@@ -101,11 +123,7 @@ function render() {
     for (let i = 0; i < game.state.length; i++) {
         const body = game.state[i]
 
-        for (let j = i + 1; j < game.state.length; j++) {
-            const body2 = game.state[j]
-            game.gravity(body, body2, i)
-        }
-
+        game.gravity(game.state, i)
         game.move(body)
     }
 
