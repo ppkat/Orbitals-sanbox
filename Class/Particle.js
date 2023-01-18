6472ea21c942b671396a64485754e6cbbd81f2fc
João Pedro <joaopedro@sobremail.com>
2023-01-17 22:05:20 -0300

    add: colision system

--- Class/Particle.js
+++ Class/Particle.js
@@ -3,10 +3,10 @@ const Body = require("./Body")
 module.exports = class Circle extends Body {
 
     constructor({ mass, color, x, y, speedx, speedy, acelerationx, acelerationy }, radius) {
-        super({mass, density: null, format: 'circle', x, y, color, speedx, speedy, acelerationx, acelerationy})
+        super({ mass, density: null, x, y, color, speedx, speedy, acelerationx, acelerationy })
         this.radius = radius
         this.area = 2 * Math.PI * this.radius
-        
+
         this.setDensity = this.mass / this.area
 
     }
--- client.js
+++ client.js
@@ -1,32 +1,28 @@
-const Circle = require("./Class/Circle")
-const Retangle = require("./Class/Retangle")
+const Particle = require("./Class/Particle")
 
 const vw = document.documentElement.clientWidth
 const vh = document.documentElement.clientHeight
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 
-canvas.width = vw * 8 / 10
-canvas.height = vh * 8 / 10
+canvas.width = vw * 9 / 10
+canvas.height = vh * 9 / 10
 canvas.style.border = '1px solid'
+canvas.style.imageRendering = 'pixalated'
 document.body.appendChild(canvas)
+let test = true
 
 function createGame() {
     const state = []
 
-    function createRetangle({ mass, color, x, y, width, height, speedx, speedy, acelerationx, acelerationy }) {
-        const retangle = new Retangle({ mass, color, x, y, speedx, speedy, acelerationx, acelerationy }, width, height)
-        state.push(retangle)
-    }
-
-    function createCircle({ mass, color, x, y, radius, speedx, speedy, acelerationx, acelerationy }) {
-        const circle = new Circle({ mass, color, x, y, speedx, speedy, acelerationx, acelerationy }, radius)
-        state.push(circle)
+    function createParticle({ mass, color, x, y, radius, speedx, speedy, acelerationx, acelerationy }) {
+        const particle = new Particle({ mass, color, x, y, speedx, speedy, acelerationx, acelerationy }, radius)
+        state.push(particle)
     }
 
     function gravity(state, i) {
         const body1 = state[i]
-        const previousBody = state[i > 0? i - 1 : state.length - 1]
+        const previousBody = state[i > 0 ? i - 1 : state.length - 1]
         previousBody.acelerationx = previousBody.acelerationy = 0 //cleaning previous aceleration
 
         for (let j = i + 1; j < state.length; j++) {
@@ -35,8 +31,8 @@ function createGame() {
             const gravityConstant = 6.67408 * 10 ** -11
             const mass1 = body1.mass
             const mass2 = body2.mass
-            const center1 = body1.format === 'circle' ? { x: body1.x, y: body1.y } : { x: body1.x + body1.width / 2, y: body1.y + body1.height / 2 }
-            const center2 = body2.format === 'circle' ? { x: body2.x, y: body2.y } : { x: body2.x + body2.width / 2, y: body2.y + body2.height / 2 }
+            const center1 = { x: body1.x, y: body1.y }
+            const center2 = { x: body2.x, y: body2.y }
             const distancex = center1.x - center2.x
             const distancey = center1.y - center2.y
 
@@ -55,6 +51,33 @@ function createGame() {
         }
     }
 
+    function gravityOnlyFirstBody() {
+
+        const body1 = game.state[0]
+        body1.acelerationx = body1.acelerationy = 0
+        for (i = 1; i < game.state.length; i++) {
+            const body2 = game.state[i]
+            const gravityConstant = 6.67408 * 10 ** -11
+            const mass1 = body1.mass
+            const mass2 = body2.mass
+            const center1 = { x: body1.x, y: body1.y }
+            const center2 = { x: body2.x, y: body2.y }
+            const distancex = center1.x - center2.x
+            const distancey = center1.y - center2.y
+
+            const distance = Math.sqrt(distancex ** 2 + distancey ** 2) // d² = (x1 - x2)² + (y1 - y2)²
+            const gravityForce = gravityConstant * mass1 * mass2 / distance ** 2 // F = G * m * m) / d²
+
+            const sin = distancey / distance
+            const cos = distancex / distance
+            const forcex = gravityForce * cos
+            const forcey = gravityForce * sin
+
+            body2.acelerationx += forcex / mass2
+            body2.acelerationy += forcey / mass2
+        }
+    }
+
     function move(body) {
 
         body.speedx += body.acelerationx
@@ -63,23 +86,73 @@ function createGame() {
         body.y += body.speedy
     }
 
-    function colision(body1, body2) {
+    function colision(state, i) {
+        const body1 = state[i]
 
-        if (body1.format === 'retangle' && body2.format === 'retangle') {
-            if (body1.x <= body2.x + body2.width && body1.x + body1.width >= body2.x && body1.y <= body2.y + body2.width && body1.y + body1.width >= body2.y) {
-                body1.speedx = -body1.speedx
-                body1.speedy = -body1.speedy
-                body2.speedx = -body2.speedx
-                body2.speedy = -body2.speedy
+        for (let j = i + 1; j < state.length; j++) {
+            const body2 = state[j]
+
+            const radiusSum = body1.radius + body2.radius
+            let distancex = body1.x - body2.x
+            let distancey = body1.y - body2.y
+            let distance = Math.hypot(distancex, distancey)
+
+            if (distance <= radiusSum) {
+                const sinFi = distancey / distance //Fi is the angle between x axis and colision center particles axis
+                const cosFi = distancex / distance
+
+                function removeOverlap() {
+                    const overlappedDistance = (radiusSum - distance) / 2;
+                    const massSum = body1.mass + body2.mass
+                    const inverseProportionality1 = body2.mass * 2 / massSum
+                    const inverseProportionality2 = body1.mass * 2 / massSum
+
+                    body1.x += overlappedDistance * cosFi * inverseProportionality1
+                    body1.y += overlappedDistance * sinFi * inverseProportionality1
+                    body2.x -= overlappedDistance * cosFi * inverseProportionality2
+                    body2.y -= overlappedDistance * sinFi * inverseProportionality2
+
+                    distance += overlappedDistance
+
+                    distancex = distance * cosFi
+                    distancey = distance * sinFi
+                    console.log('overlaped')
+                }
+
+                const isOverlap = distance < radiusSum
+                if (isOverlap) removeOverlap()
+
+                body1.colisionAxisSpeed = body1.speedx * cosFi + body1.speedy * sinFi
+                body1.complementarColisionAxisSpeed = -body1.speedx * sinFi + body1.speedy * cosFi
+                body2.colisionAxisSpeed = body2.speedx * cosFi + body2.speedy * sinFi
+                body2.complementarColisionAxisSpeed = -body2.speedx * sinFi + body2.speedy * cosFi
+
+                const newSpeed1 = ((body1.mass - body2.mass) * body1.colisionAxisSpeed + 2 * body2.mass * body2.colisionAxisSpeed) / (body1.mass + body2.mass)
+                const newSpeed2 = ((body2.mass - body1.mass) * body2.colisionAxisSpeed + 2 * body1.mass * body1.colisionAxisSpeed) / (body1.mass + body2.mass)
+
+                body1.speedx = newSpeed1 * cosFi - body1.complementarColisionAxisSpeed * sinFi
+                body1.speedy = newSpeed1 * sinFi + body1.complementarColisionAxisSpeed * cosFi
+                body2.speedx = newSpeed2 * cosFi - body2.complementarColisionAxisSpeed * sinFi
+                body2.speedy = newSpeed2 * sinFi + body2.complementarColisionAxisSpeed * cosFi
             }
         }
-        else if (body1.format === 'circle' && body2.format === 'circle') {
-            //if(body1.x + body1.width >= body2.x - body2.radius)
-        }
-        else if ((body1.format === 'circle' && body2.format === 'retangle') || (body1.format === 'retangle' && body2.format === 'circle')) {
 
-        }
-
+        // if (body1.x <= 0) {
+        //     body1.x = 0
+        //     body1.speedx = -body1.speedx
+        // }
+        // else if (body1.x >= canvas.width) {
+        //     body1.x = canvas.width
+        //     body1.speedx = -body1.speedx
+        // }
+        // else if (body1.y <= 0) {
+        //     body1.y = 0
+        //     body1.speedy = -body1.speedy
+        // }
+        // else if (body1.y >= canvas.height) {
+        //     body1.y = canvas.height
+        //     body1.speedy = -body1.speedy
+        // }
     }
 
     function draw() {
@@ -87,32 +160,44 @@ function createGame() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         for (let body of game.state) {
             ctx.fillStyle = body.color
-            if (body.format === 'retangle') {
-                ctx.fillRect(body.x, body.y, body.width, body.height)
-            } else {
-                ctx.beginPath()
-                ctx.arc(body.x, body.y, body.radius, 0, 2 * Math.PI)
-                ctx.fill()
-            }
+            ctx.beginPath()
+            ctx.arc(body.x, body.y, body.radius, 0, 2 * Math.PI)
+            ctx.fill()
         }
     }
+    // createParticle({ color: 'aqua', mass: 5.9722 * 10 ** 12, x: canvas.width / 2, y: canvas.height / 2, radius: 90, speedx: 0, speedy: 0 })
+    // createParticle({ color: 'blue', mass: 3 * 10 ** 10, x: 1000, y: 700, radius: 15, speedx: 0 })
+    // createParticle({ color: 'brown', mass: 8 * 10 ** 8, x: 800, y: 80, radius: 6, speedx: 2 })
+    // createParticle({ color: 'purple', mass: 6 * 10 ** 9, x: 800, y: 500, radius: 10, speedx: 5 })
+    // createParticle({ color: 'red', mass: 1 * 10 ** 10, x: 700, y: 500, radius: 12, speedx: -2 })
+    // createParticle({ color: 'gray', mass: 8 * 10 ** 11, x: 600, y: 400, radius: 40, speedx: 0, speedy: 2 })
+    // createParticle({ color: 'pink', mass: 4 * 10 ** 8, x: 100, y: 900, radius: 4, speedx: 0, speedy: 7 })
 
-    createRetangle({ mass: 1 * 10 ** 7, color: 'red', x: 50, y: 200, width: 30, height: 70 })
-    createRetangle({ mass: 8 * 10 ** 13, color: 'yellow', x: 400, y: 100, width: 200, height: 170 })
-    createRetangle({ mass: 5 * 10 ** 7, color: 'red', x: 200, y: 300, width: 50, height: 75 })
-    createCircle({ color: 'brown', mass: 4 * 10 ** 13, x: 800, y: 80, radius: 50, speedx: -7 })
-    createCircle({ color: 'purple', mass: 8 * 10 ** 13, x: 50, y: 500, radius: 50, speedx: 3 })
-    createCircle({ color: 'blue', mass: 1.1 * 10 ** 14, x: 1000, y: 700, radius: 85, speedx: 0 })
-    createRetangle({ mass: 9.01 * 10 ** 13, color: 'pink', x: 750, y: 300, width: 100, height: 90, speedx: 4 })
-    createCircle({ color: 'gray', mass: 6 * 10 ** 9, x: 600, y: 800, radius: 8, speedx: 3 })
+
+    for (i = 0; i < canvas.width; i += 100) {
+        for (j = 0; j < canvas.height; j += 60)
+            createParticle({ color: 'gray', mass: 8 * 10 ** 10, x: i, y: j, radius: 10 })
+        for (j = 20; j < canvas.height; j += 60)
+            createParticle({ color: 'red', mass: 6 * 10 ** 10, x: i, y: j, radius: 8 })
+        // for (j = 32; j < canvas.height; j += 60)
+        //     createParticle({ color: 'pink', mass: 4 * 10 ** 10, x: i, y: j, radius: 6 })
+        // for (j = 44; j < canvas.height; j += 60)
+        //     createParticle({ color: 'blue', mass: 2 * 10 ** 10, x: i, y: j, radius: 4 })
+    }
+
+    // for (i = 30; i < canvas.width; i += 100) {
+    //     for (j = 0; j < canvas.height; j += 60)
+    //         createParticle({ color: 'black', mass: 2 * 10 ** 8, x: i, y: j, radius: 2 })
+    // }
 
     return {
         state,
-        createCircle,
-        createRetangle,
+        createParticle,
         move,
         gravity,
-        draw
+        gravityOnlyFirstBody,
+        draw,
+        colision
     }
 }
 
@@ -120,10 +205,12 @@ const game = createGame()
 
 function render() {
 
+    //game.gravityOnlyFirstBody()
     for (let i = 0; i < game.state.length; i++) {
         const body = game.state[i]
 
         game.gravity(game.state, i)
+        game.colision(game.state, i)
         game.move(body)
     }
 
